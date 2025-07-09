cf8289725baab4dc0aad834ff0eb9d6fcdf80269
blvck93 <106159599+blvck93@users.noreply.github.com>
2025-07-09 10:38:48 +0200

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.48.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  alias  = "network_hub_us"
+  region = var.us_region
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "eu_region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "us_region" {
+  type    = string
+  default = "us-east-1"
+}
