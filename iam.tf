31559f3f25c481a5e24004e939e555fc31a1a73e
blvck93 <kamil.czarnocki@gmail.com>
2025-07-09 11:21:40 +0200

    Init

---
+++ iam.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "ciscoise" {
+  name = "ciscoiseiamrole${random_string.random_name_post.result}"
+ 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Sid = ""
+      }
+    ]
+  })
+  tags = var.tags
+}
+ 
+resource "aws_iam_role_policy" "cisco-ise-iam_role_policy" {
+  name   = "fgthaiamrolepolicy${random_string.random_name_post.result}"
+  role   = aws_iam_role.ciscoise.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+   {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:Describe*",
+        "ec2:AssociateAddress",
+        "ec2:AssignPrivateIpAddresses",
+        "ec2:UnassignPrivateIpAddresses",
+        "ec2:ReplaceRoute"
+        ],
+      "Resource": "*"
+   }
+  ]
+}
+EOF
+}
+ 
+resource "aws_iam_instance_profile" "ciscoise" {
+  name = "ciscoiseprofile${random_string.random_name_post.result}"
+ 
+  role = aws_iam_role.ciscoise.name
+  tags = var.tags
