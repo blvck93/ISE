6d99740b03bf30cb56604dadc51ddf4833c81431
blvck93 <kamil.czarnocki@gmail.com>
2025-07-09 11:06:07 +0200

    Init

---
+++ ise.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "isevm1" {
+  ami           = var.iseami
+  instance_type = var.size
+  subnet_id     = var.privatesubnetaz1
+  key_name      = "ise-key"
+  #   private_ip           = var.iseport1ip[0] # Assign static private IP
+  monitoring           = true
+  ebs_optimized        = true
+  iam_instance_profile = aws_iam_instance_profile.ciscoise.id
+  user_data_base64     = base64encode("hostname=${var.ise_vm1_hostname}\nprimarynameserver=${var.ise_dns_server}\ndnsdomain=${var.ise_domain}\nntpserver=${var.ise_ntp_server}\ntimezone=${var.ise_timezone}\nusername=${var.ise_username}\npassword=${var.ise_password}")
+
+  root_block_device {
+    delete_on_termination = true
+    volume_size           = 500
+    volume_type           = "gp3"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "enabled"
+  }
+
+  vpc_security_group_ids      = [var.vpc_security_group_id]
+  associate_public_ip_address = false
+
+  tags = var.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
--- variables.tf
+++ variables.tf
@@ -12,3 +12,62 @@ variable "us_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "ise_vm1_hostname" {
+  type    = string
+  default = "test-ise"
+}
+
+variable "ise_dns_server" {
+  type    = string
+  default = "169.254.169.253"
+}
+
+variable "ise_domain" {
+  type    = string
+  default = "example.com"
+}
+variable "ise_ntp_server" {
+  type    = string
+  default = "169.254.169.123"
+}
+
+variable "ise_timezone" {
+  type    = string
+  default = "CET"
+}
+
+variable "ise_username" {
+  type    = string
+  default = "iseadmin"
+}
+
+variable "ise_password" {
+  type    = string
+  default = "Cisco1234"
+}
+
+variable "iseami" {
+  type    = string
+  default = "ami-07946ba1cee1ca94a"
+}
+
+variable "size" {
+  type    = string
+  default = "c5.4xlarge"
+}
+
+variable "privatesubnetaz1" {
+  type    = string
+  default = "subnet-05b39f44434b0ea16"
+}
+
+variable "vpc_security_group_id" {
+  type    = string
+  default = "sg-013515ed8ce76d893"
+}
+
+variable "tags" {
+  type    = any
+  default = "Terraform"
+}
