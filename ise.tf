72b66edd0f707787a6be424b4784f8a8dd0567cd
blvck93 <kamil.czarnocki@gmail.com>
2025-07-10 12:27:14 +0200

    Reinit prod

--- ise.tf
+++ ise.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "isevm2" {
   monitoring           = true
   ebs_optimized        = true
   iam_instance_profile = aws_iam_instance_profile.ciscoise.id
-  user_data = file("ise.txt")
+  user_data = "${file("ise.txt")}"
 
   network_interface {
     network_interface_id = aws_network_interface.nic.id
@@ -21,17 +21,17 @@ resource "aws_instance" "isevm2" {
     volume_size = "700"
     device_name = "/dev/sda1"
   }
-  
+
   lifecycle {
     ignore_changes = [root_block_device,ebs_block_device]
   }
 
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "enabled"
-  }
+# metadata_options {
+#   http_endpoint               = "enabled"
+#   http_tokens                 = "required"
+#   http_put_response_hop_limit = 1
+#   instance_metadata_tags      = "enabled"
+# }
 
   #  tags = var.tags
 
