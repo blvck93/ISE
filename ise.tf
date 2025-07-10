4eb8491ddd875c6bf0bdd9dd340d5d9318d4cb9f
blvck93 <kamil.czarnocki@gmail.com>
2025-07-10 09:58:38 +0200

    Reinit

--- ise.tf
+++ ise.tf
@@ -1,38 +1,29 @@
+# Bring up ISE interfaces
 resource "aws_network_interface" "nic" {
   subnet_id       = var.privatesubnetaz1
   security_groups = [var.vpc_security_group_id]
 }
 
-resource "aws_instance" "isevm2" {
+#Bring up ISE instance
+resource "aws_instance" "ise" {
   ami                  = var.iseami
   instance_type        = var.size
   key_name             = var.keyname
-  monitoring           = true
-  ebs_optimized        = true
-  iam_instance_profile = aws_iam_instance_profile.ciscoise.id
-  user_data = file("ise.txt")
 
+  
   network_interface {
     network_interface_id = aws_network_interface.nic.id
     device_index         = 0
   }
-
-  root_block_device {
-    delete_on_termination = true
-    volume_size           = 700
-    volume_type           = "gp3"
+  tags = {
+    Name = "Test"
   }
-
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 1
-    instance_metadata_tags      = "enabled"
+  ebs_block_device {
+    volume_size = "700"
+    device_name = "/dev/sda1"
   }
-
-  #  tags = var.tags
-
-  #  lifecycle {
-  #    prevent_destroy = true
-  #  }
+  lifecycle {
+    ignore_changes = [root_block_device,ebs_block_device]
+  }
+  user_data = "${file("ise.txt")}"
 }
